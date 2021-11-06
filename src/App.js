7327875e180758b6dd866229ba1ef96ed11b23c3
Rubz07 <rubinsiby@gmail.com>
2021-11-06 10:08:28 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Components/Nav/Nav";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+   
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
